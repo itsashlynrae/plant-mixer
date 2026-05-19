9e488f3f77907cd5a92ce4a14ea52af1bca5b11d
Ashlyn <its.ashlynrae@gmail.com>
2026-05-19 15:40:32 -0600

    pushing local files

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://euuxhczn6tefcfkt....supabase.co',
+  'sb_publishable_EUUXHCzN6tefCFKt...'
+)
